0e34871c37eeb83c68c7ef36ccd965078913da87
anujageeth <www.anujageeth@gmail.com>
2024-10-18 15:41:04 +0530

    cat function

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def cat():
+    print('Meow')
+
 def default():
     print('hello')
 
 def main():
     default()
 
-    if __name__=='__main__':
+if __name__=='__main__':
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
         main()
